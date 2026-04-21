8b8ca17e6669f718eebc4eb3fec2b748df3c1b3e
莫耶尔 <12959656+fizifa@user.noreply.gitee.com>
2026-04-21 14:50:20 +0800

    修复助手函数

--- src/helpers.php
+++ src/helpers.php
@@ -22,6 +22,15 @@ if (!function_exists('qtemp\vEcho')) {
 }
 
 if (!function_exists('qtemp\getValue')) {
+    /**
+     * 安全获取数组的一系列的键的值，若键不存在则返回定义值，须知：若最终的值有效，但值等效于false，依然会返回原值
+     * 若中间有单个长度的数组会自动处理无需手动加0,若手动加0则正常进行
+     * (加这个自动的动机是thinkcmf不能远程多对一，只能用远程多对多关联，导致关联出的数据只能是数组)
+     * @param array|object $item 源数组
+     * @param string|array $keys 一系列键，可为逗号分隔的字符串或数组
+     * @param mixed $noValue 不存在时返回的值，默认为null
+     * @return mixed 
+     */
     function getValue($item, $keys = [], $noValue = null)
     {
         if (empty($item) || (!is_array($item) && !is_object($item)) || empty($keys)) {
@@ -52,6 +61,14 @@ if (!function_exists('qtemp\getValue')) {
 }
 
 if (!function_exists('qtemp\getValues')) {
+    /**
+     * 获取变量的一组数据
+     * @param array|object $item 变量值
+     * @param array $keys_arr 键数组，若提供键值对，则返回键值对数组
+     * @param array $noValue_arr 不存在时返回的值数组，若没有对应键则返回null
+     * @return array
+     * @see getValue
+     */
     function getValues($item, $keys_arr = [], $noValue_arr = [])
     {
         if (is_scalar($keys_arr)) {
@@ -67,6 +84,12 @@ if (!function_exists('qtemp\getValues')) {
 }
 
 if (!function_exists('qtemp\arrayHasKey')) {
+    /**
+     * 确保数组的键有效
+     * @param array $array
+     * @param string|array $keys
+     * @return void
+     */
     function arrayHasKey(&$array, $keys = [])
     {
         foreach ($keys as $key) {
@@ -78,31 +101,38 @@ if (!function_exists('qtemp\arrayHasKey')) {
 }
 
 if (!function_exists('qtemp\greyEcho')) {
+    /**
+     * 灰色输出，自带html处理
+     * @param string $str
+     */
     function greyEcho($str)
     {
-        if (is_string($str) && strlen($str) > 0) {
-            echo '<span style="color: #999;">', htmlspecialchars($str), '</span>';
-        }
+        echo '<span style="color:#999">' . htmlspecialchars($str) . '</span>';
     }
 }
 
 if (!function_exists('qtemp\getSubstring')) {
-    function getSubstring($string, $pattern = '*end', $noValue = false)
+    /** 
+     * 从字符串获取指定格式的子串
+     * @param string $str 源字符串
+     * @param string $replace 格式,*表示子串，不建议有多个*
+     * @return string 子串，若没找到则返回空字符串
+     */
+    function getSubstring($str, $replace)
     {
-        if ($pattern[0] === '*') {
-            $end = substr($pattern, 1);
-            $len = strlen($end);
-            if (substr($string, -$len) === $end) {
-                return substr($string, 0, -$len);
-            }
+        $parts = explode('*', $replace);
+        foreach ($parts as &$part) {
+            $part = preg_quote($part, '/');
         }
-        if (substr($pattern, -1) === '*') {
-            $start = substr($pattern, 0, -1);
-            $len = strlen($start);
-            if (substr($string, 0, $len) === $start) {
-                return substr($string, $len);
+        unset($part);
+        $pattern = '/^' . implode('(.*)', $parts) . '$/';
+        if (preg_match($pattern, $str, $matches)) {
+            $result = $matches[1];
+            if ($result === '') {
+                return '';
             }
+            return lcfirst($result);
         }
-        return $noValue;
+        return '';
     }
 }
